d9a7eab8d684cfb64cf7673bf0db7a3c5b63b567
Sebastian <sebastian.campbell@sydney.edu.au>
2013-04-08 12:04:52 +1000

    Remodeled chull to sort instead of taking 1 to length.

--- R/chBLC.R
+++ R/chBLC.R
@@ -6,18 +6,21 @@
 #' @export
 
 chBLC<- function(spectra){
+  spectra <- as.matrix(spectra)
   interval<- c(1:ncol(spectra))
   hull_spectra<- matrix(NA,ncol=ncol(spectra),nrow=nrow(spectra))
   for (i in 1:nrow(spectra)){
-    tempSpect= as.matrix(spectra[i,])
+    tempSpect <- spectra[i, ]
     data1 <- sortedXyData(interval, tempSpect)
     ## calculate convex hull
-    c_hull <- chull(data1)
+    c_hull <- sort(chull(data1))
     ## get the appropriate region
-    c_hull <- c_hull[which(c_hull == 1):length(c_hull)]
+    #c_hull <- c_hull[which(c_hull == 1):length(c_hull)]
     ## calculate linear approximation between hull points
     linear_approx <- approx(data1[c_hull,], xout = interval, method = 'linear', ties = 'mean')
     ## calculate the deviation from the convex hull
-    hull_spectra[i,] <- ( linear_approx[[2]] - tempSpect )/linear_approx[[2]]}
+    hull_spectra[i,] <- ( linear_approx[[2]] - tempSpect )/linear_approx[[2]]
+  }
   colnames(hull_spectra) <- colnames(spectra)
-  return(hull_spectra)}
+  return(hull_spectra)
+}
