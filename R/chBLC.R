ed0d30b7f42586271b0b6251c96b5bca56849d83
Sebastian <sebastian.campbell@sydney.edu.au>
2013-07-17 16:10:35 +1000

    Fixed chBLC. It now accepts cases where the hull has only two points.
    This repairs a bug in cases where the first point on the hull was the last point in the hull data.

--- R/chBLC.R
+++ R/chBLC.R
@@ -6,15 +6,20 @@
 #' @export
 
 chBLC<- function(spectra){
-  interval<- c(1:ncol(spectra))
-  hull_spectra<- matrix(NA,ncol=ncol(spectra),nrow=nrow(spectra))
-  for (i in 1:nrow(spectra)){
-    tempSpect= as.matrix(spectra[i,])
+  interval <- seq_len(ncol(spectra))
+  hull_spectra <- matrix(NA,ncol=ncol(spectra),nrow=nrow(spectra))
+  for (i in seq_len(nrow(spectra))){
+    tempSpect <- as.matrix(spectra[i,])
     data1 <- sortedXyData(interval, tempSpect)
     ## calculate convex hull
     c_hull <- chull(data1)
-    ## get the appropriate region
-    c_hull <- c_hull[which(c_hull == 1):length(c_hull)]
+    ## get the appropriate region: the points of the polygon over the spectra
+    if(tail(c_hull, 1) == 1L){
+      ## If the first point is the last point, just use the first and last point
+      c_hull <- c_hull[c(1, length(c_hull))]
+      } else {
+        c_hull <- c_hull[which(c_hull == 1):length(c_hull)]
+      }
     ## calculate linear approximation between hull points
     linear_approx <- approx(data1[c_hull,], xout = interval, method = 'linear', ties = 'mean')
     ## calculate the deviation from the convex hull
